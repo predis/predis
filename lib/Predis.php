ea9c3388daf8e4a341603a8fadaf9468803af4ca
Daniele Alessandri <suppakilla@gmail.com>
2010-04-18 12:49:38 +0200

    No more implicit serialization for an array instance passed as the single argument of a MultiBulkCommand.

--- lib/Predis.php
+++ lib/Predis.php
@@ -422,11 +422,7 @@ abstract class MultiBulkCommand extends Command {
         $argsc    = count($arguments);
 
         if ($argsc === 1 && is_array($arguments[0])) {
-            $cmd_args = array();
-            foreach ($arguments[0] as $k => $v) {
-                $cmd_args[] = $k;
-                $cmd_args[] = $v;
-            }
+            $cmd_args = $arguments[0];
             $argsc = count($cmd_args);
         }
         else {
@@ -1802,9 +1798,21 @@ class SetPreserve extends \Predis\BulkCommand {
 class SetMultiple extends \Predis\MultiBulkCommand {
     public function canBeHashed()  { return false; }
     public function getCommandId() { return 'MSET'; }
+    public function filterArguments(Array $arguments) {
+        if (count($arguments) === 1 && is_array($arguments[0])) {
+            $flattenedKVs = array();
+            $args = &$arguments[0];
+            foreach ($args as $k => $v) {
+                $flattenedKVs[] = $k;
+                $flattenedKVs[] = $v;
+            }
+            return $flattenedKVs;
+        }
+        return $arguments;
+    }
 }
 
-class SetMultiplePreserve extends \Predis\MultiBulkCommand {
+class SetMultiplePreserve extends \Predis\Commands\SetMultiple {
     public function canBeHashed()  { return false; }
     public function getCommandId() { return 'MSETNX'; }
     public function parseResponse($data) { return (bool) $data; }
