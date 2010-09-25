8bfae69bf0fda34167cde12b6a553ce0985bff7b
Daniele Alessandri <suppakilla@gmail.com>
2010-09-25 17:52:04 +0200

    Make WATCH accept an array of keys that must be watched.

--- lib/Predis.php
+++ lib/Predis.php
@@ -2850,6 +2850,12 @@ class Discard extends \Predis\MultiBulkCommand {
 class Watch extends \Predis\MultiBulkCommand {
     public function canBeHashed()  { return false; }
     public function getCommandId() { return 'WATCH'; }
+    public function filterArguments(Array $arguments) {
+        if (isset($arguments[0]) && is_array($arguments[0])) {
+            return $arguments[0];
+        }
+        return $arguments;
+    }
     public function parseResponse($data) { return (bool) $data; }
 }
 
