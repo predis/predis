2c761d6c95a2651b8b29f4aa74cce9ccd0fba3c3
Daniele Alessandri <suppakilla@gmail.com>
2011-01-02 21:55:28 +0100

    Remove a useless check for the payload length.

--- lib/Predis.php
+++ lib/Predis.php
@@ -688,16 +688,13 @@ class ResponseReader {
         }
 
         $prefix  = $header[0];
-        $payload = strlen($header) > 1 ? substr($header, 1) : '';
-
         if (!isset($this->_prefixHandlers[$prefix])) {
             Shared\Utils::onCommunicationException(new MalformedServerResponse(
                 $connection, "Unknown prefix '$prefix'"
             ));
         }
-
         $handler = $this->_prefixHandlers[$prefix];
-        return $handler->handle($connection, $payload);
+        return $handler->handle($connection, substr($header, 1));
     }
 }
 
