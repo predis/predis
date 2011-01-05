4fc5ee65fd7aba2c1d49a0c04370d02d119dc71c
Daniele Alessandri <suppakilla@gmail.com>
2011-01-05 12:47:43 +0100

    Look for a closing curly brace in a string only after the index of the opening one when using key tags.

--- lib/Predis.php
+++ lib/Predis.php
@@ -440,7 +440,7 @@ abstract class Command {
 
                 $start = strpos($key, '{');
                 if ($start !== false) {
-                    $end = strpos($key, '}');
+                    $end = strpos($key, '}', $start);
                     if ($end !== false) {
                         $key = substr($key, ++$start, $end - $start);
                     }
