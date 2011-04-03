6f51521f814ab9577767cd44fa7e65b3e8eadaa8
Daniele Alessandri <suppakilla@gmail.com>
2011-04-03 10:55:27 +0200

    Rename test file.

--- test/ClientFeaturesTest.php
+++ test/ClientFeaturesTest.php
@@ -1,6 +1,6 @@
 <?php
 
-class PredisClientFeaturesTestSuite extends PHPUnit_Framework_TestCase {
+class ClientFeaturesTestSuite extends PHPUnit_Framework_TestCase {
     public $redis;
 
     protected function setUp() {
