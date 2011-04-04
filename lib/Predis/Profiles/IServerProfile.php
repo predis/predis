27d56a7ac91b3835ad57109cc8ab3dfa90f5758b
Daniele Alessandri <suppakilla@gmail.com>
2011-04-04 11:23:38 +0200

    Methods for defining commands at runtime should not be enforced in the Predis\Profiles\IServerProfile interface.

--- lib/Predis/Profiles/IServerProfile.php
+++ lib/Predis/Profiles/IServerProfile.php
@@ -6,7 +6,5 @@ interface IServerProfile {
     public function getVersion();
     public function supportsCommand($command);
     public function supportsCommands(Array $commands);
-    public function defineCommand($command, $aliases);
-    public function defineCommands(Array $commands);
     public function createCommand($method, $arguments = array());
 }
