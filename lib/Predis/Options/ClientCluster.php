0255261cd514167f13d6eb1278f529c2858496ad
Daniele Alessandri <suppakilla@gmail.com>
2011-05-05 18:41:05 +0200

    Whoops, forgot to add Predis\Options\ClientCluster to the repository.

---
+++ lib/Predis/Options/ClientCluster.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Predis\Options;
+
+use Predis\Network\IConnectionCluster;
+use Predis\Network\PredisCluster;
+
+class ClientCluster extends Option {
+    protected function checkInstance($cluster) {
+        if (!$cluster instanceof IConnectionCluster) {
+            throw new \InvalidArgumentException(
+                'Instance of Predis\Network\IConnectionCluster expected'
+            );
+        }
+        return $cluster;
+    }
+
+    public function validate($value) {
+        if (is_callable($value)) {
+            return $this->initializeFromCallable($value);
+        }
+        $initializer = $this->getInitializer($value);
+        return $this->checkInstance($initializer());
+    }
+
+    protected function initializeFromCallable($callable) {
+        return $this->checkInstance(call_user_func($callable));
+    }
+
+    protected function getInitializer($fqnOrType) {
+        switch ($fqnOrType) {
+            case 'predis':
+                return function() { return new PredisCluster(); };
+            default:
+                return function() use($fqnOrType) {
+                    return new $fqnOrType();
+                };
+        }
+    }
+
+    public function getDefault() {
+        return new PredisCluster();
+    }
+}
