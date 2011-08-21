3733e790eb0d2b91cbcd33a903fc8c5069a2910b
Daniele Alessandri <suppakilla@gmail.com>
2011-08-21 11:55:08 +0200

    Reuse code.

--- lib/Predis/Commands/HashGetMultiple.php
+++ lib/Predis/Commands/HashGetMultiple.php
@@ -2,20 +2,14 @@
 
 namespace Predis\Commands;
 
+use Predis\Helpers;
+
 class HashGetMultiple extends Command {
     public function getId() {
         return 'HMGET';
     }
 
     protected function filterArguments(Array $arguments) {
-        if (count($arguments) === 2 && is_array($arguments[1])) {
-            $flattenedKVs = array($arguments[0]);
-            $args = $arguments[1];
-            foreach ($args as $v) {
-                $flattenedKVs[] = $v;
-            }
-            return $flattenedKVs;
-        }
-        return $arguments;
+        return Helpers::filterVariadicValues($arguments);
     }
 }
