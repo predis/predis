5680cd40b5c41e5ff8fff137f57a0489747b9e2b
Daniele Alessandri <suppakilla@gmail.com>
2011-11-18 11:03:47 +0100

    Support the different replies of CONFIG GET|SET|RESETSTAT.

--- lib/Predis/Commands/ServerConfig.php
+++ lib/Predis/Commands/ServerConfig.php
@@ -11,8 +11,12 @@
 
 namespace Predis\Commands;
 
+use Predis\Iterators\MultiBulkResponseTuple;
+
 /**
- * @link http://redis.io/commands/config
+ * @link http://redis.io/commands/config-set
+ * @link http://redis.io/commands/config-get
+ * @link http://redis.io/commands/config-resetstat
  * @author Daniele Alessandri <suppakilla@gmail.com>
  */
 class ServerConfig extends Command
@@ -39,5 +43,26 @@ class ServerConfig extends Command
     protected function canBeHashed()
     {
         return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parseResponse($data)
+    {
+        if ($data instanceof \Iterator) {
+            return new MultiBulkResponseTuple($data);
+        }
+
+        if (is_array($data)) {
+            $result = array();
+            for ($i = 0; $i < count($data); $i++) {
+                $result[$data[$i]] = $data[++$i];
+            }
+
+            return $result;
+        }
+
+        return $data;
     }
 }
