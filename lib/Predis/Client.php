0a622aee31571a380a17e55a6ca095c30dbcfe55
Daniele Alessandri <suppakilla@gmail.com>
2012-10-23 01:46:05 +0200

    Bump VERSION and update CHANGELOG.
    Guess what time is it? Yeah, once again... it is release time!

--- lib/Predis/Client.php
+++ lib/Predis/Client.php
@@ -33,7 +33,7 @@ use Predis\Transaction\MultiExecContext;
  */
 class Client implements ClientInterface
 {
-    const VERSION = '0.8.0-dev';
+    const VERSION = '0.8.0';
 
     private $options;
     private $profile;
