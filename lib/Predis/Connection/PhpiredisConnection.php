ebd895a67d51e5e364a095499c92427b82877968
Daniele Alessandri <suppakilla@gmail.com>
2013-02-09 09:49:57 +0100

    Update link to phpiredis.

--- lib/Predis/Connection/PhpiredisConnection.php
+++ lib/Predis/Connection/PhpiredisConnection.php
@@ -41,7 +41,7 @@ use Predis\Command\CommandInterface;
  *  - timeout: timeout to perform the connection.
  *  - read_write_timeout: timeout of read / write operations.
  *
- * @link http://github.com/seppo0010/phpiredis
+ * @link http://github.com/nrk/phpiredis
  * @author Daniele Alessandri <suppakilla@gmail.com>
  */
 class PhpiredisConnection extends AbstractConnection
