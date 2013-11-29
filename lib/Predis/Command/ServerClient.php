5856bde1982fe188e81c0df046bbcd9ee0d181de
Daniele Alessandri <suppakilla@gmail.com>
2013-11-29 11:14:06 +0100

    Fix phpdoc.
    [ci skip]

--- lib/Predis/Command/ServerClient.php
+++ lib/Predis/Command/ServerClient.php
@@ -12,7 +12,10 @@
 namespace Predis\Command;
 
 /**
- * @link http://redis.io/commands/client
+ * @link http://redis.io/commands/client-list
+ * @link http://redis.io/commands/client-kill
+ * @link http://redis.io/commands/client-getname
+ * @link http://redis.io/commands/client-setname
  * @author Daniele Alessandri <suppakilla@gmail.com>
  */
 class ServerClient extends AbstractCommand
