d4be306d0aca28b5633b96adef03b29fea569c2f
Daniele Alessandri <suppakilla@gmail.com>
2014-08-01 11:59:50 +0200

    ... and here comes Predis v1.0.0!

--- src/Client.php
+++ src/Client.php
@@ -42,7 +42,7 @@ use Predis\Transaction\MultiExec as MultiExecTransaction;
  */
 class Client implements ClientInterface
 {
-    const VERSION = '1.0.0-dev';
+    const VERSION = '1.0.0';
 
     protected $connection;
     protected $options;
