65c2f4bc4fb7609ee73d69aa88313be11ea275ef
Daniele Alessandri <suppakilla@gmail.com>
2014-08-08 13:57:05 +0200

    Back to development.
    [ci skip]

--- src/Client.php
+++ src/Client.php
@@ -42,7 +42,7 @@ use Predis\Transaction\MultiExec as MultiExecTransaction;
  */
 class Client implements ClientInterface
 {
-    const VERSION = '1.0.0';
+    const VERSION = '1.1.0-dev';
 
     protected $connection;
     protected $options;
