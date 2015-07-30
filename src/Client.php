84060b9034d756b4d79641667d7f9efe1aeb8e04
Daniele Alessandri <suppakilla@gmail.com>
2015-07-30 20:34:15 +0200

    Update CHANGELOG and bump VERSION.
    Hotfix release targeting a severe regression on HHVM, see commit d4a7bd5.

--- src/Client.php
+++ src/Client.php
@@ -40,7 +40,7 @@ use Predis\Transaction\MultiExec as MultiExecTransaction;
  */
 class Client implements ClientInterface
 {
-    const VERSION = '1.0.3-dev';
+    const VERSION = '1.0.3';
 
     protected $connection;
     protected $options;
