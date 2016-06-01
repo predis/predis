0e17edbefb50c6cbd1acc4a6f6ef06399deb1af2
Daniele Alessandri <suppakilla@gmail.com>
2016-06-02 00:06:21 +0200

    Update CHANGELOG and bump VERSION.

--- src/Client.php
+++ src/Client.php
@@ -40,7 +40,7 @@ use Predis\Transaction\MultiExec as MultiExecTransaction;
  */
 class Client implements ClientInterface, \IteratorAggregate
 {
-    const VERSION = '1.1.0-dev';
+    const VERSION = '1.1.0';
 
     protected $connection;
     protected $options;
