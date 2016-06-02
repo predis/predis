3dccd6bf87584e885e297dc10cf8f5a5ffd75177
Daniele Alessandri <suppakilla@gmail.com>
2016-06-02 09:10:25 +0200

    Back to development.
    The master branch now hosts the development of Predis v2.0.0-dev.

--- src/Client.php
+++ src/Client.php
@@ -40,7 +40,7 @@ use Predis\Transaction\MultiExec as MultiExecTransaction;
  */
 class Client implements ClientInterface, \IteratorAggregate
 {
-    const VERSION = '1.1.0';
+    const VERSION = '2.0.0-dev';
 
     protected $connection;
     protected $options;
