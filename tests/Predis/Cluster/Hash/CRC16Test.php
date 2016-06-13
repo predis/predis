4d58928efde0b88455ffb05f84b6669b35b06fcc
Daniele Alessandri <suppakilla@gmail.com>
2016-06-13 18:57:06 +0200

    [tests] Add test for Predis\Cluster\Hash\CRC16.

---
+++ tests/Predis/Cluster/Hash/CRC16Test.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) Daniele Alessandri <suppakilla@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Cluster\Hash;
+
+use PredisTestCase;
+
+/**
+ *
+ */
+class CRC16Test extends PredisTestCase
+{
+    /**
+     * @group disconnected
+     */
+    public function testHashGeneration()
+    {
+        $crc16 = new CRC16();
+
+        $this->assertSame(58359, $crc16->hash('key:000'));
+        $this->assertSame(62422, $crc16->hash('key:001'));
+        $this->assertSame(50101, $crc16->hash('key:002'));
+        $this->assertSame(54164, $crc16->hash('key:003'));
+        $this->assertSame(41843, $crc16->hash('key:004'));
+        $this->assertSame(45906, $crc16->hash('key:005'));
+        $this->assertSame(33585, $crc16->hash('key:006'));
+        $this->assertSame(37648, $crc16->hash('key:007'));
+        $this->assertSame(25343, $crc16->hash('key:008'));
+        $this->assertSame(29406, $crc16->hash('key:009'));
+    }
+}
