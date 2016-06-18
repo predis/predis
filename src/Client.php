2d4377fc81c513388668be39509fc673174b3189
Daniele Alessandri <suppakilla@gmail.com>
2016-06-18 21:37:42 +0200

    Back to development.

--- src/Client.php
+++ src/Client.php
@@ -40,7 +40,7 @@ use Predis\Transaction\MultiExec as MultiExecTransaction;
  */
 class Client implements ClientInterface, \IteratorAggregate
 {
-    const VERSION = '1.1.1';
+    const VERSION = '1.1.2-dev';
 
     protected $connection;
     protected $options;
