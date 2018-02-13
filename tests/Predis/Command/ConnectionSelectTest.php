2db5e4724243e8150acc9f62356167c27994ab6e
Remi Collet <remi@remirepo.net>
2018-02-13 10:42:16 +0100

    fix #485 test failure with recent redis version

--- tests/Predis/Command/ConnectionSelectTest.php
+++ tests/Predis/Command/ConnectionSelectTest.php
@@ -71,7 +71,7 @@ class ConnectionSelectTest extends PredisCommandTestCase
     /**
      * @group connected
      * @expectedException \Predis\Response\ServerException
-     * @expectedExceptionMessage ERR invalid DB index
+     * @expectedExceptionMessage DB index
      */
     public function testThrowsExceptionOnUnexpectedDatabase()
     {
