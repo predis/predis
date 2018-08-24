59222a34b941f34ec2181bbfa18ab1ada2a7e731
hncg <checgg@gmail.com>
2018-08-24 14:16:33 +0800

    .env for password

--- src/Connection/Factory.php
+++ src/Connection/Factory.php
@@ -173,7 +173,7 @@ class Factory implements FactoryInterface
     {
         $parameters = $connection->getParameters();
 
-        if (empty($parameters->password)) {
+        if (!empty($parameters->password)) {
             $connection->addConnectCommand(
                 new RawCommand(array('AUTH', $parameters->password))
             );
