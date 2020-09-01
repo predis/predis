0fdca9e80528aacbe886307279c58514b5dd5e45
Daniele Alessandri <suppakilla@gmail.com>
2020-09-01 20:39:34 +0200

    Remove check for PHP <= 5.4 from Predis\Session\Handler.

--- src/Session/Handler.php
+++ src/Session/Handler.php
@@ -48,18 +48,7 @@ class Handler implements \SessionHandlerInterface
      */
     public function register()
     {
-        if (PHP_VERSION_ID >= 50400) {
-            session_set_save_handler($this, true);
-        } else {
-            session_set_save_handler(
-                array($this, 'open'),
-                array($this, 'close'),
-                array($this, 'read'),
-                array($this, 'write'),
-                array($this, 'destroy'),
-                array($this, 'gc')
-            );
-        }
+        session_set_save_handler($this, true);
     }
 
     /**
