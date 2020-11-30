0d8460672307af8b4f7cf50f1428f03d562b3802
Robert O'Rourke <rob@humanmade.com>
2020-11-30 11:07:54 +0000

    Switch to old array style syntax to support PHP 5.3

--- tests/apply-patches.php
+++ tests/apply-patches.php
@@ -3,11 +3,11 @@
  * Apply patches to support newer PHP versions.
  */
 
-$patches = [
+$patches = array(
     'phpunit_mock_objects.patch' => 'phpunit/phpunit-mock-objects',
     'phpunit_php7.patch' => 'phpunit/phpunit',
     'phpunit_php8.patch' => 'phpunit/phpunit',
-];
+);
 
 foreach ($patches as $patch => $package) {
     chdir(__DIR__.'/../vendor/'.$package);
