bb9a670c83dbf15db7ed7ddcbda16d32e902a082
Till Krüss <till@kruss.io>
2021-09-29 10:43:13 -0700

    add php 8.1 patches for phpunit

--- tests/apply-patches.php
+++ tests/apply-patches.php
@@ -7,6 +7,7 @@ $patches = array(
     'phpunit_mock_objects.patch' => 'phpunit/phpunit-mock-objects',
     'phpunit_php7.patch' => 'phpunit/phpunit',
     'phpunit_php8.patch' => 'phpunit/phpunit',
+    'phpunit_php81.patch' => 'phpunit/phpunit',
 );
 
 foreach ($patches as $patch => $package) {
