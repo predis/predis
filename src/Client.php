4aa3014bb0201753aaa8860bad0730c9d690adc8
Till Krüss <till@kruss.io>
2021-10-05 11:55:45 -0700

    tag 1.1.9

--- src/Client.php
+++ src/Client.php
@@ -40,7 +40,7 @@ use Predis\Transaction\MultiExec as MultiExecTransaction;
  */
 class Client implements ClientInterface, \IteratorAggregate
 {
-    const VERSION = '1.1.9-dev';
+    const VERSION = '1.1.9';
 
     protected $connection;
     protected $options;
