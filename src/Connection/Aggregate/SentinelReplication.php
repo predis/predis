97dce00f720a9e23a0d6ad43dd2b783ccdb09daa
Ghost <Leeloo5E@users.noreply.github.com>
2022-01-19 11:54:59 +0100

    Use Sentinel with authentication
    closes #594

--- src/Connection/Aggregate/SentinelReplication.php
+++ src/Connection/Aggregate/SentinelReplication.php
@@ -248,7 +248,7 @@ class SentinelReplication implements ReplicationInterface
             // in a later release.
             $parameters['database'] = null;
             $parameters['username'] = null;
-            $parameters['password'] = null;
+//             $parameters['password'] = null;
 
             if (!isset($parameters['timeout'])) {
                 $parameters['timeout'] = $this->sentinelTimeout;
