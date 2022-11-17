94bd54b2afabced2a5be7b37b20eb749fd4c8d2b
vladvildanov <117659936+vladvildanov@users.noreply.github.com>
2022-11-17 17:48:16 +0200

    Extend Sorted Set support by implementing ZMSCORE (#823)

---
+++ src/Command/Redis/ZMSCORE.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Predis\Command\Redis;
+
+use Predis\Command\Command as RedisCommand;
+
+/**
+ * @link https://redis.io/commands/zmscore/
+ *
+ * Returns the scores associated with the specified members
+ * in the sorted set stored at key.
+ *
+ * For every member that does not exist in the sorted set, a null value is returned.
+ *
+ * @version >= 6.2.0
+ */
+class ZMSCORE extends RedisCommand
+{
+    /**
+     * @inheritDoc
+     */
+    public function getId()
+    {
+        return 'ZMSCORE';
+    }
+}
