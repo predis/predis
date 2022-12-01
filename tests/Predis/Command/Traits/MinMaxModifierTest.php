13c0c40a869ed5507fb3ee7255492cd0647c6b8e
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2022-12-01 15:32:17 +0200

    Added support for ZMPOP command

---
+++ tests/Predis/Command/Traits/MinMaxModifierTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Predis\Command\Traits;
+
+use PredisTestCase;
+use Predis\Command\Command as RedisCommand;
+use UnexpectedValueException;
+
+class MinMaxModifierTest extends PredisTestCase
+{
+    private $testClass;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->testClass = new class extends RedisCommand {
+            use MinMaxModifier;
+
+            public function getId()
+            {
+                return 'test';
+            }
+        };
+    }
+
+    /**
+     * @dataProvider argumentsProvider
+     * @param int $offset
+     * @param array $actualArguments
+     * @param array $expectedArguments
+     * @return void
+     */
+    public function testResolveModifierModifyArrayCorrect(
+        int $offset,
+        array $actualArguments,
+        array $expectedArguments
+    ): void {
+        $this->testClass->resolveModifier($offset, $actualArguments);
+        $this->assertSame($expectedArguments, $actualArguments);
+    }
+
+    public function testThrowsExceptionOnWrongModifierValue(): void
+    {
+        $arguments = ['argument1', 'wrong modifier'];
+
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage('Wrong type of modifier given');
+
+        $this->testClass->resolveModifier(1, $arguments);
+    }
+
+    public function argumentsProvider(): array
+    {
+        return [
+            'with modifier' => [
+                0,
+                ['max'],
+                ['MAX']
+            ],
+            'without modifier' => [
+                2,
+                ['argument1', 'argument2'],
+                ['argument1', 'argument2', 'MIN']
+            ]
+        ];
+    }
+}
