41060de83d8dabdb18063827223a9d2a4490d7cf
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2022-12-12 10:39:30 +0200

    Added command link and description

--- src/Command/Redis/BZPOPMIN.php
+++ src/Command/Redis/BZPOPMIN.php
@@ -5,6 +5,16 @@ namespace Predis\Command\Redis;
 use Predis\Command\Traits\Keys;
 use Predis\Command\Command as RedisCommand;
 
+/**
+ * @link https://redis.io/commands/bzpopmin/
+ *
+ * BZPOPMIN is the blocking variant of the sorted set ZPOPMIN primitive.
+ *
+ * It is the blocking version because it blocks the connection when there are
+ * no members to pop from any of the given sorted sets.
+ * A member with the lowest score is popped from first sorted set that is non-empty,
+ * with the given keys being checked in the order that they are given.
+ */
 class BZPOPMIN extends RedisCommand
 {
     use Keys {
