e45c0a455bb112c6f820acb38a076966090cf12b
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2022-12-19 15:31:36 +0200

    Added decorator to check Redis JSON module version

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -86,6 +86,7 @@ use Predis\Response\Status;
  * @method int               hsetnx(string $key, string $field, string $value)
  * @method array             hvals(string $key)
  * @method int               hstrlen(string $key, string $field)
+ * @method array             jsonget(string $key, string $indent = '', string $newline = '', string $space = '', string ...$paths)
  * @method string            jsonset(string $key, string $path, string $value, ?string $subcommand = null)
  * @method string            blmove(string $source, string $destination, string $where, string $to, int $timeout)
  * @method array|null        blpop(array|string $keys, int|float $timeout)
