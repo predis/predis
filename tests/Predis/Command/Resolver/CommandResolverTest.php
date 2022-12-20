84a536364ca2a383371fc43bdb4af1c32dc819fc
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2022-12-20 15:26:56 +0200

    Re-written CommandResolver test

--- tests/Predis/Command/Resolver/CommandResolverTest.php
+++ tests/Predis/Command/Resolver/CommandResolverTest.php
@@ -9,17 +9,15 @@ class CommandResolverTest extends TestCase
 {
     /**
      * @dataProvider commandsProvider
-     * @param array $modules
      * @param string $commandID
      * @param string|null $expectedCommandClass
      * @return void
      */
     public function testResolveResolvesCorrectlyCommand(
-        array $modules,
         string $commandID,
         ?string $expectedCommandClass
     ): void {
-        $resolver = new CommandResolver($modules);
+        $resolver = new CommandResolver();
 
         $this->assertSame($expectedCommandClass, $resolver->resolve($commandID));
     }
@@ -27,9 +25,9 @@ class CommandResolverTest extends TestCase
     public function commandsProvider(): array
     {
         return [
-            'core command exists' => [[], 'SET', SET::class],
-            'module not exist' => [[['name' => 'foo', 'commandPrefix' => 'bar']], 'BARFOO', null],
-            'module exists, module command not exists' => [[['name' => 'foo', 'commandPrefix' => 'bar']], 'FOOBAR', null],
+            'core command exists' => ['SET', SET::class],
+            'module not exist' => ['FOOBAR', null],
+            'module exists, module command not exists' => ['JSONFOO', null],
         ];
     }
 }
