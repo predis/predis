c855daea20c7457a0633a44b1f65e5f191cf3578
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2022-12-20 14:36:24 +0200

    Added test coverage for JSON.GET command

---
+++ tests/Predis/Command/Traits/Json/NewlineTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Predis\Command\Traits\Json;
+use Predis\Command\Command as RedisCommand;
+use PredisTestCase;
+use UnexpectedValueException;
+
+class NewlineTest extends PredisTestCase
+{
+    private $testClass;
+
+    protected function setUp(): void
+    {
+        $this->testClass = new class extends RedisCommand {
+            use Newline;
+
+            public static $newlineArgumentPositionOffset = 0;
+
+            public function getId()
+            {
+                return 'test';
+            }
+        };
+    }
+
+    /**
+     * @dataProvider argumentsProvider
+     * @param int $offset
+     * @param array $actualArguments
+     * @param array $expectedResponse
+     * @return void
+     */
+    public function testReturnsCorrectArguments(
+        int $offset,
+        array $actualArguments,
+        array $expectedResponse
+    ): void {
+        $this->testClass::$newlineArgumentPositionOffset = $offset;
+
+        $this->testClass->setArguments($actualArguments);
+
+        $this->assertSame($expectedResponse, $this->testClass->getArguments());
+    }
+
+    /**
+     * @return void
+     */
+    public function testThrowsExceptionOnUnexpectedValueGiven(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage("Newline argument value should be a string");
+
+        $this->testClass->setArguments([1]);
+    }
+
+    public function argumentsProvider(): array
+    {
+        return [
+            'with wrong offset' => [
+                2,
+                ['argument1'],
+                ['argument1'],
+            ],
+            'with default value' => [
+                0,
+                [''],
+                [false]
+            ],
+            'with correct argument' => [
+                0,
+                ['\n'],
+                ['NEWLINE', '\n'],
+            ],
+        ];
+    }
+}
