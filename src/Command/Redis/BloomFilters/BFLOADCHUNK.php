b069359adc34aa086f383195137409f9c335c6be
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2022-12-30 14:23:18 +0200

    Added support for BF.LOADCHUNK command

---
+++ src/Command/Redis/BloomFilters/BFLOADCHUNK.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Predis\Command\Redis\BloomFilters;
+
+use Predis\Command\Command as RedisCommand;
+
+/**
+ * @link https://redis.io/commands/bf.loadchunk/
+ *
+ * Restores a filter previously saved using SCANDUMP. See the SCANDUMP command for example usage.
+ */
+class BFLOADCHUNK extends RedisCommand
+{
+    public function getId()
+    {
+        return 'BF.LOADCHUNK';
+    }
+}
