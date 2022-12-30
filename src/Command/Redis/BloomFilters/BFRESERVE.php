f693f8394c51b650488c92c4154e749efacc1901
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2022-12-30 11:36:36 +0200

    Added command description

--- src/Command/Redis/BloomFilters/BFRESERVE.php
+++ src/Command/Redis/BloomFilters/BFRESERVE.php
@@ -5,6 +5,15 @@ namespace Predis\Command\Redis\BloomFilters;
 use Predis\Command\Command as RedisCommand;
 use Predis\Command\Traits\BloomFilters\Expansion;
 
+/**
+ * @link https://redis.io/commands/bf.reserve/
+ *
+ * Creates an empty Bloom Filter with a single sub-filter for the initial capacity
+ * requested and with an upper bound error_rate.
+ *
+ * By default, the filter auto-scales by creating additional sub-filters when capacity is reached.
+ * The new sub-filter is created with size of the previous sub-filter multiplied by expansion.
+ */
 class BFRESERVE extends RedisCommand
 {
     use Expansion {
