f4bd6d44b17c0d77671c08652074db7f190f4f49
Till Krüss <till@kruss.io>
2023-01-17 14:42:59 -0800

    update cs-fixer config

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,33 @@
+<?php
+
+$PREDIS_HEADER = <<<EOS
+This file is part of the Predis package.
+
+(c) 2009-2020 Daniele Alessandri
+(c) 2021-2023 Till Krüss
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+EOS;
+
+return (new PhpCsFixer\Config)
+    ->setUsingCache(false)
+    ->setRules([
+        'header_comment' => ['header' => $PREDIS_HEADER],
+        '@Symfony' => true,
+        'phpdoc_separation' => false,
+        'phpdoc_annotation_without_dot' => false,
+        'no_superfluous_phpdoc_tags' => false,
+        'global_namespace_import' => true,
+        'yoda_style' => false,
+        'single_line_throw' => false,
+        'concat_space' => ['spacing' => 'one'],
+        'increment_style' => ['style' => 'post'],
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__ . '/bin')
+            ->in(__DIR__ . '/examples')
+            ->in(__DIR__ . '/src')
+            ->in(__DIR__ . '/tests')
+    );
