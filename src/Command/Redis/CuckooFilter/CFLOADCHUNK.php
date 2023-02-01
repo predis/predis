c11174f750024ac84df257e6fbb9ad80871b7244
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2023-02-01 10:17:44 +0200

    Added support for CF.LOADCHUNK, CF.SCANDUMP commands

---
+++ src/Command/Redis/CuckooFilter/CFLOADCHUNK.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Command\Redis\CuckooFilter;
+
+use Predis\Command\Command as RedisCommand;
+
+/**
+ * @see https://redis.io/commands/cf.loadchunk/
+ *
+ * Restores a filter previously saved using SCANDUMP.
+ * See the SCANDUMP command for example usage.
+ */
+class CFLOADCHUNK extends RedisCommand
+{
+    public function getId()
+    {
+        return 'CF.LOADCHUNK';
+    }
+}
---
+++ tests/Predis/Command/Redis/CuckooFilter/CFSCANDUMP_Test.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Command\Redis\CuckooFilter;
+
+use Predis\Command\Redis\PredisCommandTestCase;
+use Predis\Response\ServerException;
+
+class CFSCANDUMP_Test extends PredisCommandTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getExpectedCommand(): string
+    {
+        return CFSCANDUMP::class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getExpectedId(): string
+    {
+        return 'CFSCANDUMP';
+    }
+
+    /**
+     * @group disconnected
+     */
+    public function testFilterArguments(): void
+    {
+        $actualArguments = ['key', 1];
+        $expectedArguments = ['key', 1];
+
+        $command = $this->getCommand();
+        $command->setArguments($actualArguments);
+
+        $this->assertSameValues($expectedArguments, $command->getArguments());
+    }
+
+    /**
+     * @group disconnected
+     */
+    public function testParseResponse(): void
+    {
+        $this->assertSame(1, $this->getCommand()->parseResponse(1));
+    }
+
+    /**
+     * @group connected
+     * @return void
+     * @requiresRedisBfVersion >= 1.0.0
+     */
+    public function testScanDumpReturnsNotEmptyDataChunk(): void
+    {
+        $expectedIterator = 1;
+        $redis = $this->getClient();
+
+        $redis->cfadd('key', 'item1');
+        [$iterator, $dataChunk] = $redis->cfscandump('key', 0);
+
+        $this->assertSame($expectedIterator, $iterator);
+        $this->assertNotEmpty($dataChunk);
+    }
+
+    /**
+     * @group connected
+     * @requiresRedisBfVersion >= 1.0.0
+     */
+    public function testThrowsExceptionOnWrongType(): void
+    {
+        $this->expectException(ServerException::class);
+        $this->expectExceptionMessage('Operation against a key holding the wrong kind of value');
+
+        $redis = $this->getClient();
+
+        $redis->set('cfscandump_foo', 'bar');
+        $redis->cfscandump('cfscandump_foo', 0);
+    }
+}
