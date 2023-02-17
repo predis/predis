5ab4d85da66fe7bf3886a54de47d6254699655dd
Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
2023-02-17 15:34:51 +0200

    Added support for FT.SPELLCHECK command

--- src/Command/Argument/Search/SearchArguments.php
+++ src/Command/Argument/Search/SearchArguments.php
@@ -20,7 +20,7 @@ class SearchArguments implements ArrayableArgument
     /**
      * @var array
      */
-    private $arguments;
+    private $arguments = [];
 
     /**
      * @var string[]
@@ -28,6 +28,22 @@ class SearchArguments implements ArrayableArgument
     private $supportedDataTypesEnum = [
         'hash' => 'HASH',
         'json' => 'JSON',
+    ];
+
+    /**
+     * @var string[]
+     */
+    private $sortingEnum = [
+        'asc' => 'ASC',
+        'desc' => 'DESC',
+    ];
+
+    /**
+     * @var string[]
+     */
+    private $termsEnum = [
+        'include' => 'INCLUDE',
+        'exclude' => 'EXCLUDE',
     ];
 
     /**
@@ -243,6 +259,480 @@ class SearchArguments implements ArrayableArgument
     public function skipInitialScan(): self
     {
         $this->arguments[] = 'SKIPINITIALSCAN';
+
+        return $this;
+    }
+
+    /**
+     * Returns the document ids and not the content.
+     *
+     * @return $this
+     */
+    public function noContent(): self
+    {
+        $this->arguments[] = 'NOCONTENT';
+
+        return $this;
+    }
+
+    /**
+     * Does not try to use stemming for query expansion but searches the query terms verbatim.
+     *
+     * @return $this
+     */
+    public function verbatim(): self
+    {
+        $this->arguments[] = 'VERBATIM';
+
+        return $this;
+    }
+
+    /**
+     * Also returns the relative internal score of each document.
+     *
+     * @return $this
+     */
+    public function withScores(): self
+    {
+        $this->arguments[] = 'WITHSCORES';
+
+        return $this;
+    }
+
+    /**
+     * Retrieves optional document payloads.
+     *
+     * @return $this
+     */
+    public function withPayloads(): self
+    {
+        $this->arguments[] = 'WITHPAYLOADS';
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of the sorting key, right after the id and score and/or payload, if requested.
+     *
+     * @return $this
+     */
+    public function withSortKeys(): self
+    {
+        $this->arguments[] = 'WITHSORTKEYS';
+
+        return $this;
+    }
+
+    /**
+     * Limits results to those having numeric values ranging between min and max,
+     * if numeric_attribute is defined as a numeric attribute in FT.CREATE.
+     * Min and max follow ZRANGE syntax, and can be -inf, +inf, and use( for exclusive ranges.
+     * Multiple numeric filters for different attributes are supported in one query.
+     *
+     * @param  array ...$filter Should contain: numeric_field, min and max. Example: ['numeric_field', 1, 10]
+     * @return $this
+     */
+    public function searchFilter(array ...$filter): self
+    {
+        $arguments = func_get_args();
+
+        foreach ($arguments as $argument) {
+            array_push($this->arguments, 'FILTER', ...$argument);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Filter the results to a given radius from lon and lat. Radius is given as a number and units.
+     *
+     * @param  array ...$filter Should contain: geo_field, lon, lat, radius, unit. Example: ['geo_field', 34.1231, 35.1231, 300, km]
+     * @return $this
+     */
+    public function geoFilter(array ...$filter): self
+    {
+        $arguments = func_get_args();
+
+        foreach ($arguments as $argument) {
+            array_push($this->arguments, 'GEOFILTER', ...$argument);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Limits the result to a given set of keys specified in the list.
+     *
+     * @param  array $keys
+     * @return $this
+     */
+    public function inKeys(array $keys): self
+    {
+        $this->arguments[] = 'INKEYS';
+        $this->arguments[] = count($keys);
+        $this->arguments = array_merge($this->arguments, $keys);
+
+        return $this;
+    }
+
+    /**
+     * Filters the results to those appearing only in specific attributes of the document, like title or URL.
+     *
+     * @param  array $fields
+     * @return $this
+     */
+    public function inFields(array $fields): self
+    {
+        $this->arguments[] = 'INFIELDS';
+        $this->arguments[] = count($fields);
+        $this->arguments = array_merge($this->arguments, $fields);
+
+        return $this;
+    }
+
+    /**
+     * Limits the attributes returned from the document.
+     * Num is the number of attributes following the keyword.
+     * If num is 0, it acts like NOCONTENT.
+     * Identifier is either an attribute name (for hashes and JSON) or a JSON Path expression (for JSON).
+     * Property is an optional name used in the result. If not provided, the identifier is used in the result.
+     *
+     * If you want to add alias property to your identifier just add "true" value in identifier enumeration,
+     * next value will be considered as alias to previous one.
+     *
+     * Example: 'identifier', true, 'property' => 'identifier' AS 'property'
+     *
+     * @param  int         $count
+     * @param  string|bool ...$identifier
+     * @return $this
+     */
+    public function addReturn(int $count, ...$identifier): self
+    {
+        $arguments = func_get_args();
+
+        $this->arguments[] = 'RETURN';
+
+        for ($i = 1, $iMax = count($arguments); $i < $iMax; $i++) {
+            if (true === $arguments[$i]) {
+                $arguments[$i] = 'AS';
+            }
+        }
+
+        $this->arguments = array_merge($this->arguments, $arguments);
+
+        return $this;
+    }
+
+    /**
+     * Returns only the sections of the attribute that contain the matched text.
+     *
+     * @param  array  $fields
+     * @param  int    $frags
+     * @param  int    $len
+     * @param  string $separator
+     * @return $this
+     */
+    public function summarize(array $fields = [], int $frags = 0, int $len = 0, string $separator = ''): self
+    {
+        $this->arguments[] = 'SUMMARIZE';
+
+        if (!empty($fields)) {
+            $this->arguments[] = 'FIELDS';
+            $this->arguments[] = count($fields);
+            $this->arguments = array_merge($this->arguments, $fields);
+        }
+
+        if ($frags !== 0) {
+            $this->arguments[] = 'FRAGS';
+            $this->arguments[] = $frags;
+        }
+
+        if ($len !== 0) {
+            $this->arguments[] = 'LEN';
+            $this->arguments[] = $len;
+        }
+
+        if ($separator !== '') {
+            $this->arguments[] = 'SEPARATOR';
+            $this->arguments[] = $separator;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Formats occurrences of matched text.
+     *
+     * @param  array  $fields
+     * @param  string $openTag
+     * @param  string $closeTag
+     * @return $this
+     */
+    public function highlight(array $fields = [], string $openTag = '', string $closeTag = ''): self
+    {
+        $this->arguments[] = 'HIGHLIGHT';
+
+        if (!empty($fields)) {
+            $this->arguments[] = 'FIELDS';
+            $this->arguments[] = count($fields);
+            $this->arguments = array_merge($this->arguments, $fields);
+        }
+
+        if ($openTag !== '' && $closeTag !== '') {
+            array_push($this->arguments, 'TAGS', $openTag, $closeTag);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Allows a maximum of N intervening number of unmatched offsets between phrase terms.
+     * In other words, the slop for exact phrases is 0.
+     *
+     * @param  int   $slop
+     * @return $this
+     */
+    public function slop(int $slop): self
+    {
+        $this->arguments[] = 'SLOP';
+        $this->arguments[] = $slop;
+
+        return $this;
+    }
+
+    /**
+     * Overrides the timeout parameter of the module.
+     *
+     * @param  int   $timeout
+     * @return $this
+     */
+    public function timeout(int $timeout): self
+    {
+        $this->arguments[] = 'TIMEOUT';
+        $this->arguments[] = $timeout;
+
+        return $this;
+    }
+
+    /**
+     * Puts the query terms in the same order in the document as in the query, regardless of the offsets between them.
+     * Typically used in conjunction with SLOP.
+     *
+     * @return $this
+     */
+    public function inOrder(): self
+    {
+        $this->arguments[] = 'INORDER';
+
+        return $this;
+    }
+
+    /**
+     * Uses a custom query expander instead of the stemmer.
+     *
+     * @param  string $expander
+     * @return $this
+     */
+    public function expander(string $expander): self
+    {
+        $this->arguments[] = 'EXPANDER';
+        $this->arguments[] = $expander;
+
+        return $this;
+    }
+
+    /**
+     * Uses a custom scoring function you define.
+     *
+     * @param  string $scorer
+     * @return $this
+     */
+    public function scorer(string $scorer): self
+    {
+        $this->arguments[] = 'SCORER';
+        $this->arguments[] = $scorer;
+
+        return $this;
+    }
+
+    /**
+     * Returns a textual description of how the scores were calculated.
+     * Using this options requires the WITHSCORES option.
+     *
+     * @return $this
+     */
+    public function explainScore(): self
+    {
+        $this->arguments[] = 'EXPLAINSCORE';
+
+        return $this;
+    }
+
+    /**
+     * Adds an arbitrary, binary safe payload that is exposed to custom scoring functions.
+     *
+     * @param  string $payload
+     * @return $this
+     */
+    public function payload(string $payload): self
+    {
+        $this->arguments[] = 'PAYLOAD';
+        $this->arguments[] = $payload;
+
+        return $this;
+    }
+
+    /**
+     * Orders the results by the value of this attribute.
+     * This applies to both text and numeric attributes.
+     * Attributes needed for SORTBY should be declared as SORTABLE in the index, in order to be available with very low latency.
+     * Note that this adds memory overhead.
+     *
+     * @param  string $sortAttribute
+     * @param  string $orderBy
+     * @return $this
+     */
+    public function sortBy(string $sortAttribute, string $orderBy = 'asc'): self
+    {
+        $this->arguments[] = 'SORTBY';
+        $this->arguments[] = $sortAttribute;
+
+        if (in_array(strtoupper($orderBy), $this->sortingEnum)) {
+            $this->arguments[] = $this->sortingEnum[strtolower($orderBy)];
+        } else {
+            $enumValues = implode(', ', array_values($this->sortingEnum));
+            throw new InvalidArgumentException("Wrong order direction value given. Currently supports: {$enumValues}");
+        }
+
+        return $this;
+    }
+
+    /**
+     * Adds an arbitrary, binary safe payload that is exposed to custom scoring functions.
+     *
+     * @param  int   $offset
+     * @param  int   $num
+     * @return $this
+     */
+    public function limit(int $offset, int $num): self
+    {
+        array_push($this->arguments, 'LIMIT', $offset, $num);
+
+        return $this;
+    }
+
+    /**
+     * Defines one or more value parameters. Each parameter has a name and a value.
+     *
+     * Example: ['name1', 'value1', 'name2', 'value2'...]
+     *
+     * @param  array $nameValuesDictionary
+     * @return $this
+     */
+    public function params(array $nameValuesDictionary): self
+    {
+        $this->arguments[] = 'PARAMS';
+        $this->arguments[] = count($nameValuesDictionary);
+        $this->arguments = array_merge($this->arguments, $nameValuesDictionary);
+
+        return $this;
+    }
+
+    /**
+     * Selects the dialect version under which to execute the query.
+     * If not specified, the query will execute under the default dialect version
+     * set during module initial loading or via FT.CONFIG SET command.
+     *
+     * @param  string $dialect
+     * @return $this
+     */
+    public function dialect(string $dialect): self
+    {
+        $this->arguments[] = 'DIALECT';
+        $this->arguments[] = $dialect;
+
+        return $this;
+    }
+
+    /**
+     * Adds search context.
+     *
+     * @return $this
+     */
+    public function search(): self
+    {
+        $this->arguments[] = 'SEARCH';
+
+        return $this;
+    }
+
+    /**
+     * Adds aggregate context.
+     *
+     * @return $this
+     */
+    public function aggregate(): self
+    {
+        $this->arguments[] = 'AGGREGATE';
+
+        return $this;
+    }
+
+    /**
+     * Removes details of reader iterator.
+     *
+     * @return $this
+     */
+    public function limited(): self
+    {
+        $this->arguments[] = 'LIMITED';
+
+        return $this;
+    }
+
+    /**
+     * Is query string, as if sent to FT.SEARCH.
+     *
+     * @param  string $query
+     * @return $this
+     */
+    public function query(string $query): self
+    {
+        $this->arguments[] = 'QUERY';
+        $this->arguments[] = $query;
+
+        return $this;
+    }
+
+    /**
+     * Is maximum Levenshtein distance for spelling suggestions (default: 1, max: 4).
+     *
+     * @return $this
+     */
+    public function distance(int $distance): self
+    {
+        $this->arguments[] = 'DISTANCE';
+        $this->arguments[] = $distance;
+
+        return $this;
+    }
+
+    /**
+     * Specifies an inclusion (INCLUDE) or exclusion (EXCLUDE) of a custom dictionary named {dict}.
+     *
+     * @param  string $dictionary
+     * @param  string $modifier
+     * @param  string ...$terms
+     * @return $this
+     */
+    public function terms(string $dictionary, string $modifier = 'INCLUDE', string ...$terms): self
+    {
+        if (!in_array(strtoupper($modifier), $this->termsEnum)) {
+            $enumValues = implode(', ', array_values($this->termsEnum));
+            throw new InvalidArgumentException("Wrong modifier value given. Currently supports: {$enumValues}");
+        }
+
+        array_push($this->arguments, 'TERMS', $this->termsEnum[strtolower($modifier)], $dictionary, ...$terms);
 
         return $this;
     }
--- tests/Predis/Command/Argument/Search/SearchArgumentsTest.php
+++ tests/Predis/Command/Argument/Search/SearchArgumentsTest.php
@@ -201,7 +201,343 @@ class SearchArgumentsTest extends TestCase
     /**
      * @return void
      */
-    public function testCreatesCorrectArgumentsSetOnMethodsChainCall(): void
+    public function testCreatesArgumentsWithNoContentModifier(): void
+    {
+        $this->arguments->noContent();
+
+        $this->assertSame(['NOCONTENT'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithVerbatimModifier(): void
+    {
+        $this->arguments->verbatim();
+
+        $this->assertSame(['VERBATIM'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithWithScoresModifier(): void
+    {
+        $this->arguments->withScores();
+
+        $this->assertSame(['WITHSCORES'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithWithPayloadsModifier(): void
+    {
+        $this->arguments->withPayloads();
+
+        $this->assertSame(['WITHPAYLOADS'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithWithSortKeysModifier(): void
+    {
+        $this->arguments->withSortKeys();
+
+        $this->assertSame(['WITHSORTKEYS'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithSearchFilterModifier(): void
+    {
+        $this->arguments->searchFilter(['numeric_field', 1, 10], ['numeric_field1', 2, 5]);
+
+        $this->assertSame(
+            ['FILTER', 'numeric_field', 1, 10, 'FILTER', 'numeric_field1', 2, 5],
+            $this->arguments->toArray()
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithGeoFilterModifier(): void
+    {
+        $this->arguments->geoFilter(
+            ['geo_field', 13.2321, 14.2321, 300, 'km'],
+            ['geo_field1', 15.231, 16.234, 210, 'km']
+        );
+
+        $this->assertSame(
+            ['GEOFILTER', 'geo_field', 13.2321, 14.2321, 300, 'km', 'GEOFILTER', 'geo_field1', 15.231, 16.234, 210, 'km'],
+            $this->arguments->toArray()
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithInKeysModifier(): void
+    {
+        $this->arguments->inKeys(['key1', 'key2']);
+
+        $this->assertSame(['INKEYS', 2, 'key1', 'key2'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithInFieldsModifier(): void
+    {
+        $this->arguments->inFields(['field1', 'field2']);
+
+        $this->assertSame(['INFIELDS', 2, 'field1', 'field2'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithReturnModifier(): void
+    {
+        $this->arguments->addReturn(2, 'identifier', true, 'property', 'identifier2', 'identifier3');
+
+        $this->assertSame(
+            ['RETURN', 2, 'identifier', 'AS', 'property', 'identifier2', 'identifier3'],
+            $this->arguments->toArray()
+        );
+    }
+
+    /**
+     * @dataProvider summarizeProvider
+     * @param  array $arguments
+     * @param  array $expectedResponse
+     * @return void
+     */
+    public function testCreatesArgumentsWithSummarizeModifier(array $arguments, array $expectedResponse): void
+    {
+        $this->arguments->summarize(...$arguments);
+
+        $this->assertSame($expectedResponse, $this->arguments->toArray());
+    }
+
+    /**
+     * @dataProvider highlightProvider
+     * @param  array $arguments
+     * @param  array $expectedResponse
+     * @return void
+     */
+    public function testCreatesArgumentsWithHighlightModifier(array $arguments, array $expectedResponse): void
+    {
+        $this->arguments->highlight(...$arguments);
+
+        $this->assertSame($expectedResponse, $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithSlopModifier(): void
+    {
+        $this->arguments->slop(2);
+
+        $this->assertSame(['SLOP', 2], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithTimeoutModifier(): void
+    {
+        $this->arguments->timeout(2);
+
+        $this->assertSame(['TIMEOUT', 2], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithInOrderModifier(): void
+    {
+        $this->arguments->inOrder();
+
+        $this->assertSame(['INORDER'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithExpanderModifier(): void
+    {
+        $this->arguments->expander('expander');
+
+        $this->assertSame(['EXPANDER', 'expander'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithScorerModifier(): void
+    {
+        $this->arguments->scorer('scorer');
+
+        $this->assertSame(['SCORER', 'scorer'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsExplainScoreModifier(): void
+    {
+        $this->arguments->explainScore();
+
+        $this->assertSame(['EXPLAINSCORE'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithPayloadModifier(): void
+    {
+        $this->arguments->payload('payload');
+
+        $this->assertSame(['PAYLOAD', 'payload'], $this->arguments->toArray());
+    }
+
+    /**
+     * @dataProvider sortByProvider
+     * @param  array $arguments
+     * @param  array $expectedResponse
+     * @return void
+     */
+    public function testCreatesArgumentsWithSortByModifier(array $arguments, array $expectedResponse): void
+    {
+        $this->arguments->sortBy(...$arguments);
+
+        $this->assertSame($expectedResponse, $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testThrowsExceptionOnSortByWrongOrderByModifier(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Wrong order direction value given. Currently supports: ASC, DESC');
+
+        $this->arguments->sortBy('sort_attribute', 'wrong');
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithLimitModifier(): void
+    {
+        $this->arguments->limit(2, 2);
+
+        $this->assertSame(['LIMIT', 2, 2], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithParamsModifier(): void
+    {
+        $this->arguments->params(['name1', 'value1', 'name2', 'value2']);
+
+        $this->assertSame(['PARAMS', 4, 'name1', 'value1', 'name2', 'value2'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithDialectModifier(): void
+    {
+        $this->arguments->dialect('dialect');
+
+        $this->assertSame(['DIALECT', 'dialect'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithSearchModifier(): void
+    {
+        $this->arguments->search();
+
+        $this->assertSame(['SEARCH'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithAggregateModifier(): void
+    {
+        $this->arguments->aggregate();
+
+        $this->assertSame(['AGGREGATE'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithLimitedModifier(): void
+    {
+        $this->arguments->limited();
+
+        $this->assertSame(['LIMITED'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithQueryModifier(): void
+    {
+        $this->arguments->query('query');
+
+        $this->assertSame(['QUERY', 'query'], $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesArgumentsWithDistanceModifier(): void
+    {
+        $this->arguments->distance(2);
+
+        $this->assertSame(['DISTANCE', 2], $this->arguments->toArray());
+    }
+
+    /**
+     * @dataProvider termsProvider
+     * @param  array $arguments
+     * @param  array $expectedResponse
+     * @return void
+     */
+    public function testCreatesArgumentsWithTermsModifier(array $arguments, array $expectedResponse): void
+    {
+        $this->arguments->terms(...$arguments);
+
+        $this->assertSame($expectedResponse, $this->arguments->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testThrowsExceptionOnInvalidTermsModifierValue(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Wrong modifier value given. Currently supports: INCLUDE, EXCLUDE');
+
+        $this->arguments->terms('dict', 'wrong');
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesCorrectFTCreateArgumentsSetOnMethodsChainCall(): void
     {
         $this->arguments->prefix(['prefix:', 'prefix1:']);
         $this->arguments->filter('@age>16');
@@ -211,5 +547,105 @@ class SearchArgumentsTest extends TestCase
             ['PREFIX', 2, 'prefix:', 'prefix1:', 'FILTER', '@age>16', 'STOPWORDS', 2, 'hello', 'world'],
             $this->arguments->toArray()
         );
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreatesCorrectFTSearchArgumentsSetOnMethodsChainCall(): void
+    {
+        $this->arguments->withScores();
+        $this->arguments->withPayloads();
+        $this->arguments->searchFilter(['numeric_field', 1, 10]);
+        $this->arguments->addReturn(2, 'identifier', true, 'property');
+
+        $this->assertSame(
+            ['WITHSCORES', 'WITHPAYLOADS', 'FILTER', 'numeric_field', 1, 10, 'RETURN', 2, 'identifier', 'AS', 'property'],
+            $this->arguments->toArray()
+        );
+    }
+
+    public function summarizeProvider(): array
+    {
+        return [
+            'with no arguments' => [
+                [],
+                ['SUMMARIZE'],
+            ],
+            'with fields only' => [
+                [['field1', 'field2']],
+                ['SUMMARIZE', 'FIELDS', 2, 'field1', 'field2'],
+            ],
+            'with non-default FRAGS' => [
+                [['field1', 'field2'], 2],
+                ['SUMMARIZE', 'FIELDS', 2, 'field1', 'field2', 'FRAGS', 2],
+            ],
+            'with non-default LEN' => [
+                [['field1', 'field2'], 0, 2],
+                ['SUMMARIZE', 'FIELDS', 2, 'field1', 'field2', 'LEN', 2],
+            ],
+            'with non-default SEPARATOR' => [
+                [['field1', 'field2'], 0, 0, ','],
+                ['SUMMARIZE', 'FIELDS', 2, 'field1', 'field2', 'SEPARATOR', ','],
+            ],
+            'with all arguments' => [
+                [['field1', 'field2'], 2, 2, ','],
+                ['SUMMARIZE', 'FIELDS', 2, 'field1', 'field2', 'FRAGS', 2, 'LEN', 2, 'SEPARATOR', ','],
+            ],
+        ];
+    }
+
+    public function highlightProvider(): array
+    {
+        return [
+            'with no arguments' => [
+                [],
+                ['HIGHLIGHT'],
+            ],
+            'with fields only' => [
+                [['field1', 'field2']],
+                ['HIGHLIGHT', 'FIELDS', 2, 'field1', 'field2'],
+            ],
+            'with open tag only' => [
+                [['field1', 'field2'], 'openTag'],
+                ['HIGHLIGHT', 'FIELDS', 2, 'field1', 'field2'],
+            ],
+            'with close tag only' => [
+                [['field1', 'field2'], '', 'closeTag'],
+                ['HIGHLIGHT', 'FIELDS', 2, 'field1', 'field2'],
+            ],
+            'with both tags' => [
+                [['field1', 'field2'], 'openTag', 'closeTag'],
+                ['HIGHLIGHT', 'FIELDS', 2, 'field1', 'field2', 'TAGS', 'openTag', 'closeTag'],
+            ],
+        ];
+    }
+
+    public function sortByProvider(): array
+    {
+        return [
+            'with default arguments' => [
+                ['sort_attribute'],
+                ['SORTBY', 'sort_attribute', 'ASC'],
+            ],
+            'with DESC modifier' => [
+                ['sort_attribute', 'desc'],
+                ['SORTBY', 'sort_attribute', 'DESC'],
+            ],
+        ];
+    }
+
+    public function termsProvider(): array
+    {
+        return [
+            'with INCLUDE modifier' => [
+                ['dict', 'INCLUDE', 'term1', 'term2'],
+                ['TERMS', 'INCLUDE', 'dict', 'term1', 'term2'],
+            ],
+            'with EXCLUDE modifier' => [
+                ['dict', 'EXCLUDE', 'term1', 'term2'],
+                ['TERMS', 'EXCLUDE', 'dict', 'term1', 'term2'],
+            ],
+        ];
     }
 }
