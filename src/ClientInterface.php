16844693029d19867dd78df6300a29b2b1a6bafa
Vladyslav Vildanov <117659936+vladvildanov@users.noreply.github.com>
2023-02-23 14:33:41 +0200

    Extended RediSearch support by implementing FT.DICTDEL support (#1156)
    * add support for CF.ADDNX
    
    * fix key name
    
    * fix wrong command
    
    * Pulling changes
    
    * Added support for FT.CREATE command
    
    * Fixed tests to choose correct DB
    
    * Added test coverage
    
    * Revert changes for missing commands
    
    * Added data types enums, added methods default assignments
    
    * Fixed vector field, removed default assignments, fixed tests
    
    * Added constants enum for Sortable argument, renamed arguments object
    
    * Codestyle fixes
    
    * Rename test class
    
    * Added support for FT.DICTADD command
    
    * Added support for FT.DICTDEL command
    
    ---------
    
    Co-authored-by: shacharPash <shachar.pashchur@redis.com>
    Co-authored-by: Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -101,6 +101,7 @@ use Predis\Response\Status;
  * @method Status            ftaliasupdate(string $alias, string $index)
  * @method Status            ftcreate(string $index, Schema $schema, ?CreateArguments $arguments = null)
  * @method int               ftdictadd(string $dict, ...$term)
+ * @method int               ftdictdel(string $dict, ...$term)
  * @method array             ftinfo(string $index)
  * @method array             ftsearch(string $index, string $query, ?SearchArguments $arguments = null)
  * @method string|null       get(string $key)
