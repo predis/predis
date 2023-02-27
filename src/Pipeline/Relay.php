736a94d82bec6f6e737726cc818fe5e4c4c85dbb
Zaher Ghaibeh <zaher@zaher.dev>
2023-02-27 14:00:13 +0300

    initial work on the tx group

---
+++ src/Pipeline/Relay.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Pipeline;
+
+use InvalidArgumentException;
+use Predis\Connection\ConnectionInterface;
+use SplQueue;
+
+/**
+ * Command pipeline that writes commands to the servers but discards responses.
+ */
+class Relay extends Pipeline
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function executePipeline(ConnectionInterface $connection, SplQueue $commands)
+    {
+        $pipeline = $connection->getClient()->pipeline();
+
+        foreach ($commands as $command) {
+           $pipeline->rawCommand($command->getId(), ...$command->getArguments());
+        }
+
+        return $pipeline->exec();
+    }
+}
