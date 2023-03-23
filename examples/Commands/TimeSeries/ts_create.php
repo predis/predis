c88d8ebce3f6764c2efea2c24129b9912ade9f04
Vladyslav Vildanov <117659936+vladvildanov@users.noreply.github.com>
2023-03-23 09:41:35 +0200

    Extended TimeSeries support by implementing TS.CREATE command (#1197)
    * Added support for new arguments for BITPOS, BITCOUNT commands (#1045)
    
    * Added support for new arguments for EXPIRE, EXPIREAT commands (#1046)
    
    * Extended core support by implementing SORT_RO command (#1044)
    
    * Added support for SORT_RO command
    
    * Codestyle fixes
    
    * Added command description
    
    ---------
    
    Co-authored-by: Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
    
    * fix deprecated call
    
    * Added support for container commands (#1049)
    
    * Added support for container commands FUNCTION LOAD, FUNCTION DELETE and FCALL
    
    * Changed ContainerInterface and AbstractContainer
    
    * Re-implement logic of abstract methods
    
    ---------
    
    Co-authored-by: Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
    
    * Added stream commands to KeyPrefixProcessor (#1051)
    
    Co-authored-by: Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
    
    * Fix return type of ReplicationInterface::getSlaves (#1111)
    
    * Codestyle fixes
    
    * Changed return annotation
    
    * Added support for Time Series commands, TSCREATE command
    
    * Fixed param annotation
    
    ---------
    
    Co-authored-by: Vladyslav Vildanov <vladyslavvildanov@Vladyslav-Vildanov-MacBook-Pro.local>
    Co-authored-by: Till Krüss <till@kruss.io>
    Co-authored-by: Stephan <glaubinix@users.noreply.github.com>

---
+++ examples/Commands/TimeSeries/ts_create.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Predis\Client;
+use Predis\Command\Argument\TimeSeries\CommonArguments;
+use Predis\Command\Argument\TimeSeries\CreateArguments;
+
+require __DIR__ . '/../../shared.php';
+
+// Example of TS.CREATE command usage:
+
+// Create time series
+$client = new Client();
+
+$arguments = (new CreateArguments())
+    ->retention(60000)
+    ->duplicatePolicy(CommonArguments::POLICY_MAX)
+    ->labels('sensor_id', 2, 'area_id', 32);
+
+$response = $client->tscreate('temperature:2:32', $arguments);
+
+echo "Time series creation status: {$response}";
