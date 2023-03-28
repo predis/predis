2c41e2328d376a1a1e934ceeb72635c1ccf37f02
Till Krüss <till@kruss.io>
2023-03-27 20:16:39 -0700

    add relay example

---
+++ examples/relay_events.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . '/shared.php';
+
+$key = null;
+
+$client = new Predis\Client($single_server, [
+    'connections' => 'relay',
+]);
+
+$relay = $client->getConnection()->getClient();
+
+// establish connection
+$client->ping();
+
+// register FLUSH callback
+$relay->onFlushed(
+    static function (Relay\Event $event) use (&$key) {
+        echo 'Redis was flushed, unsetting $key...' . PHP_EOL;
+        $key = null;
+    }
+);
+
+// register INVALIDATE callback
+$relay->onInvalidated(
+    static function (Relay\Event $event) use (&$key) {
+        if ($event->key === 'library') {
+            echo "The `{$event->key}` key was invalidated, unsetting \$key..." . PHP_EOL;
+            $key = null;
+        }
+    }
+);
+
+// Write key to Redis
+$client->set('library', 'relay');
+
+// Retrieve key from Redis (cached in Relay and $key)
+$key = $client->get('library');
+
+while (true) {
+    // $relay->dispatchEvents();
+    echo '$key is: ' . var_export($key, true) . PHP_EOL;
+
+    sleep(1);
+}
