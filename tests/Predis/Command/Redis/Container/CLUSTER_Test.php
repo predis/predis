2f486444465e12c4b4c4a8b2e0624c2234e33566
vladvildanov <divinez122@outlook.com>
2023-08-08 16:42:27 +0300

    Codestyle fixes

---
+++ tests/Predis/Command/Redis/Container/CLUSTER_Test.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Command\Redis\Container;
+
+use Predis\ClientInterface;
+use PredisTestCase;
+
+class CLUSTER_Test extends PredisTestCase
+{
+    public function testGetContainerCommandId(): void
+    {
+        $mockClient = $this->getMockBuilder(ClientInterface::class)->getMock();
+        $command = new CLUSTER($mockClient);
+
+        $this->assertSame('CLUSTER', $command->getContainerCommandId());
+    }
+}
