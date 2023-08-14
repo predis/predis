311cd2b79ec0d134294d723851075865b00d91d4
Vladyslav Vildanov <117659936+vladvildanov@users.noreply.github.com>
2023-08-14 09:18:19 +0300

    Added support for WAITAOF command (#1357)
    * Added support for WAITAOF command
    
    * Added call to parent setUp method

---
+++ tests/Predis/Command/Redis/WAITAOF_Test.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Predis package.
+ *
+ * (c) 2009-2020 Daniele Alessandri
+ * (c) 2021-2023 Till Krüss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Predis\Command\Redis;
+
+class WAITAOF_Test extends PredisCommandTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $redis = $this->getClient();
+        $this->assertEquals('OK', $redis->config('set', 'appendonly', 'yes'));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getExpectedCommand(): string
+    {
+        return WAITAOF::class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getExpectedId(): string
+    {
+        return 'WAITAOF';
+    }
+
+    /**
+     * @group disconnected
+     */
+    public function testFilterArguments(): void
+    {
+        $actualArguments = $expectedArguments = [1, 2, 3];
+
+        $command = $this->getCommand();
+        $command->setArguments($actualArguments);
+
+        $this->assertSameValues($expectedArguments, $command->getArguments());
+    }
+
+    /**
+     * @group disconnected
+     */
+    public function testParseResponse(): void
+    {
+        $this->assertSame(1, $this->getCommand()->parseResponse(1));
+    }
+
+    /**
+     * @group connected
+     * @return void
+     * @requiresRedisVersion >= 7.2.0
+     */
+    public function testReturnQuantityOfSyncedAOFInstances(): void
+    {
+        $redis = $this->getClient();
+
+        $this->assertEquals('OK', $redis->set('foo', 'bar'));
+        $this->assertSame([1, 0], $redis->waitaof(1, 0, 0));
+    }
+
+    protected function tearDown(): void
+    {
+        $redis = $this->getClient();
+        $this->assertEquals('OK', $redis->config('set', 'appendonly', 'no'));
+    }
+}
