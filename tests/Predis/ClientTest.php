4a7e1182b266d41807401403928e644f563d477d
vladvildanov <divinez122@outlook.com>
2023-12-27 11:48:15 +0200

    Codestyle fixes

--- tests/Predis/ClientTest.php
+++ tests/Predis/ClientTest.php
@@ -957,7 +957,7 @@ class ClientTest extends PredisTestCase
      */
     public function testGetClientByMethodSupportsSelectingConnectionByCommand(): void
     {
-        $command = \Predis\Command\RawCommand::create('GET', 'key');
+        $command = Command\RawCommand::create('GET', 'key');
         $connection = $this->getMockBuilder('Predis\Connection\ConnectionInterface')->getMock();
 
         $aggregate = $this->getMockBuilder('Predis\Connection\AggregateConnectionInterface')
--- tests/Predis/Configuration/Option/ConnectionsTest.php
+++ tests/Predis/Configuration/Option/ConnectionsTest.php
@@ -36,7 +36,7 @@ class ConnectionsTest extends PredisTestCase
      */
     public function testAcceptsNamedArrayWithSchemeToConnectionClassMappings(): void
     {
-        /** @var \Predis\Configuration\OptionsInterface */
+        /** @var OptionsInterface */
         $options = $this->getMockBuilder('Predis\Configuration\OptionsInterface')->getMock();
 
         $class = get_class($this->getMockBuilder('Predis\Connection\NodeConnectionInterface')->getMock());
@@ -184,7 +184,7 @@ class ConnectionsTest extends PredisTestCase
     {
         $option = new Connections();
 
-        /** @var \Predis\Configuration\OptionsInterface */
+        /** @var OptionsInterface */
         $options = $this->getMockBuilder('Predis\Configuration\OptionsInterface')->getMock();
 
         $callable = $this->getMockBuilder('stdClass')
@@ -211,7 +211,7 @@ class ConnectionsTest extends PredisTestCase
 
         $option = new Connections();
 
-        /** @var \Predis\Configuration\OptionsInterface */
+        /** @var OptionsInterface */
         $options = $this->getMockBuilder('Predis\Configuration\OptionsInterface')->getMock();
 
         $option->filter($options, new stdClass());
