e769301d3b173280bac24df0b3e851ab77297fd8
Denny Septian Panggabean <xamidimura@gmail.com>
2024-11-06 09:56:39 +0700

    fix: conditional using above PHP7.4

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,5 @@
 <?php
 
-use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
-
 $PREDIS_HEADER = <<<EOS
 This file is part of the Predis package.
 
@@ -12,24 +10,23 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 EOS;
 
-return (new PhpCsFixer\Config)
-    ->setParallelConfig(ParallelConfigFactory::detect())
-    ->setRules([
-        '@PHP71Migration' => true,
-        'header_comment' => ['header' => $PREDIS_HEADER],
-        '@Symfony' => true,
-        'phpdoc_separation' => false,
-        'phpdoc_annotation_without_dot' => false,
-        'no_superfluous_phpdoc_tags' => false,
-        'no_unneeded_curly_braces' => false,
-        'no_unneeded_braces' => false,
-        'global_namespace_import' => true,
-        'yoda_style' => false,
-        'single_line_throw' => false,
-        'concat_space' => ['spacing' => 'one'],
-        'increment_style' => false,
-        'trailing_comma_in_multiline' => ['after_heredoc' => true, 'elements' => ['array_destructuring', 'arrays']]
-    ])
+$fixer = new PhpCsFixer\Config;
+$fixer->setRules([
+    '@PHP71Migration' => true,
+    'header_comment' => ['header' => $PREDIS_HEADER],
+    '@Symfony' => true,
+    'phpdoc_separation' => false,
+    'phpdoc_annotation_without_dot' => false,
+    'no_superfluous_phpdoc_tags' => false,
+    'no_unneeded_curly_braces' => false,
+    'no_unneeded_braces' => false,
+    'global_namespace_import' => true,
+    'yoda_style' => false,
+    'single_line_throw' => false,
+    'concat_space' => ['spacing' => 'one'],
+    'increment_style' => false,
+    'trailing_comma_in_multiline' => ['after_heredoc' => true, 'elements' => ['array_destructuring', 'arrays']]
+])
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->in(__DIR__ . '/bin')
@@ -37,3 +34,9 @@ return (new PhpCsFixer\Config)
             ->in(__DIR__ . '/src')
             ->in(__DIR__ . '/tests')
     );
+
+if (PHP_VERSION_ID >= 70400) {
+    $fixer->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
+}
+
+return $fixer;
