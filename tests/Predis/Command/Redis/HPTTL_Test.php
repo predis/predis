7631d317fd58bb3d1b8b4144b461e94f0a786bd9
Till Krüss <till@kruss.io>
2024-11-21 11:37:03 -0800

    update year

--- tests/Predis/Command/Redis/HPTTL_Test.php
+++ tests/Predis/Command/Redis/HPTTL_Test.php
@@ -4,7 +4,7 @@
  * This file is part of the Predis package.
  *
  * (c) 2009-2020 Daniele Alessandri
- * (c) 2021-2023 Till Krüss
+ * (c) 2021-2024 Till Krüss
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
