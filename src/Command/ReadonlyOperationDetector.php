059ad92796df329b8b8d88c15a20f17764b0b67f
Alexandr Hacicheant <a.hacicheant@gmail.com>
2025-01-10 20:01:55 +0200

    Code style fixes

--- src/Command/ReadonlyOperationDetector.php
+++ src/Command/ReadonlyOperationDetector.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * This file is part of the Predis package.
  *
  * (c) 2009-2020 Daniele Alessandri
- * (c) 2021-2023 Till Krüss
+ * (c) 2021-2024 Till Krüss
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
