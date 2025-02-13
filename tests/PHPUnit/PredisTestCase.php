140497732f4a81cacb83f6ee8577823e5af21a24
vladvildanov <vladyslav.vildanov@redis.com>
2025-02-13 14:10:15 +0200

    Merge stack tests into main test job

--- tests/PHPUnit/PredisTestCase.php
+++ tests/PHPUnit/PredisTestCase.php
@@ -175,7 +175,7 @@ abstract class PredisTestCase extends PHPUnit\Framework\TestCase
         return [
             'scheme' => 'tcp',
             'host' => constant('REDIS_SERVER_HOST'),
-            'port' => constant('REDIS_SERVER_PORT'),
+            'port' => ($this->isStackTest()) ? constant('REDIS_STACK_SERVER_PORT') : constant('REDIS_SERVER_PORT'),
             'database' => constant('REDIS_SERVER_DBNUM'),
         ];
     }
@@ -582,6 +582,21 @@ abstract class PredisTestCase extends PHPUnit\Framework\TestCase
             && !empty($annotations['method']['requiresRedisVersion'])
             && in_array('connected', $annotations['method']['group'], true)
             && in_array('cluster', $annotations['method']['group'], true);
+    }
+
+    /**
+     * Check annotations if it's matches to stack test scenario.
+     *
+     * @return bool
+     */
+    protected function isStackTest(): bool
+    {
+        $annotations = TestUtil::parseTestMethodAnnotations(
+            get_class($this),
+            $this->getName(false)
+        );
+
+        return in_array('realm-stack', $annotations['class']['group'], true);
     }
 
     /**
