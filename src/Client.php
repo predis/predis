f999120ff156306dc0003745fd1695c1ed70b4da
Till Krüss <tillkruss@users.noreply.github.com>
2025-05-02 08:31:47 -0700

    prepare 3.0 release

--- src/Client.php
+++ src/Client.php
@@ -54,7 +54,7 @@ use Traversable;
  */
 class Client implements ClientInterface, IteratorAggregate
 {
-    public const VERSION = '3.0.0-RC1';
+    public const VERSION = '3.0.0';
 
     /** @var OptionsInterface */
     private $options;
