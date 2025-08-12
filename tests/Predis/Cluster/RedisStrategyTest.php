0f4bc7653c2e046850a03c2aa11873c15a97e2a2
Róbert Kelčák <RobiNN1@users.noreply.github.com>
2025-08-12 12:33:48 +0200

    Fix stream commands cluster support (#1587)
    * Fix stream commands cluster support
    
    * Update CHANGELOG.md

--- tests/Predis/Cluster/RedisStrategyTest.php
+++ tests/Predis/Cluster/RedisStrategyTest.php
@@ -497,6 +497,11 @@ class RedisStrategyTest extends PredisTestCase
             'HSCAN' => 'keys-first',
             'HSTRLEN' => 'keys-first',
 
+            /* commands operating on streams */
+            'XADD' => 'keys-first',
+            'XDEL' => 'keys-first',
+            'XRANGE' => 'keys-first',
+
             /* commands operating on HyperLogLog */
             'PFADD' => 'keys-first',
             'PFCOUNT' => 'keys-all',
