3c92b313e8854db1b46cb30b0ed7a8d06a45d25f
Till Krüss <tillkruss@users.noreply.github.com>
2026-02-23 11:45:12 -0800

    Bump version from 3.4.0 to 3.4.1

--- src/Client.php
+++ src/Client.php
@@ -56,7 +56,7 @@ use Traversable;
  */
 class Client implements ClientInterface, IteratorAggregate
 {
-    public const VERSION = '3.4.0';
+    public const VERSION = '3.4.1';
 
     /** @var OptionsInterface */
     private $options;
